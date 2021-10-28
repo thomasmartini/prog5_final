82c6c431287dd748b00a06bba2ccdfd2d9c30dfb
thoma <thomasmartini1999@gmail.com>
2021-10-29 00:24:29 +0200

    forum blade can read posts from database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('forum');
+    return view('forum', [
+        'posts' => Post::all()
+
+    ]);
 });

5249ca5edc461c4348c77b7186a18874ac029790
thoma <thomasmartini1999@gmail.com>
2021-11-11 11:59:34 +0100

    added validation to registration page

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -19,8 +20,8 @@ class RegisterController extends Controller
 $attributes = \request()->validate([
 
     'name' => 'required|max:255',
-    'username' => 'required|max:255',
-    'email' => 'required|email|max:255',
+    'username' => ['required', 'max:255', Rule::unique('users', 'username')],
+    'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email' )],
     'password'=> 'required|max:255'
 ]);
 User::create($attributes);
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,21 +5,7 @@
 
             <form method="POST" action="/register" class="mt-10">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppservase font-bold text-small text-gray-700"
-                           for="username"
-                    >
-                        username
 
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           placeholder="username"
-                           name="username"
-                           id="username"
-                           required
-                    >
-                </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppservase font-bold text-small text-gray-700"
                            for="name"
@@ -32,8 +18,33 @@
                            placeholder="name"
                            name="name"
                            id="name"
+                           value="{{old('name')}}"
                            required
                     >
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+
+                <div class="mb-6 mt-6">
+                    <label class="block mb-2 uppservase font-bold text-small text-gray-700"
+                           for="username"
+                    >
+                        username
+
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           placeholder="username"
+                           name="username"
+                           id="username"
+                           value="{{old('username')}}"
+                           required
+                    >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+                </div>
+
                     <div class="mb-6 mt-6">
                     <label class="block mb-2 uppservase font-bold text-small text-gray-700"
                            for="email"
@@ -46,8 +57,12 @@
                            placeholder="email"
                            name="email"
                            id="email"
+                           value="{{old('email')}}"
                            required
                     >
+                        @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
                 </div>
                     <div class="mb-6">
                 <label class="block mb-2 uppservase font-bold text-small text-gray-700"
@@ -63,7 +78,9 @@
                        id="password"
                        required
                 >
-
+                        @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror
                 <div class="mb-6">
                     <button type="submit"
                             class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mt-4">
